9957e0e8b8c7e3e5f550b139cd6f1fb73fd9d388
Ivan Kuznetsov <i_kuznetsov@lesta.group>
2025-11-04 15:05:23 +0300

    separate metriks event

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './styles/index.scss';
 import './assets/styles/media.scss';
+import {Helmet} from "react-helmet";
 import Index from './pages/MainPage';
 import PopupsContainer from '$popup-service/components/PopupsContainer';
 import Loader from '$common/Loader';
@@ -8,6 +9,11 @@ import Loader from '$common/Loader';
 function App() {
   return (
     <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>My Title</title>
+        <link rel="canonical" href="http://mysite.com/example" />
+      </Helmet>
       <Loader/>
       <PopupsContainer />
       <Index />
